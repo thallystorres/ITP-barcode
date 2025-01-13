43ec016c7dd8f71f87c07ca63606b8fa64dfb590
joaoMiguelSousaOliveira <jjoaomiguel05@gmail.com>
2025-01-13 15:52:04 -0300

    tirando comentários

--- verificador.c
+++ verificador.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 // Função para calcular o dígito verificador
-int calcularDigitoVerificador(int ean[7]) {
+int calcularDigitoVerificador(int ean[7])
+{
     int soma = 0;
-    
+
     // Calcula a soma dos dígitos com os pesos
-    for (int i = 0; i < 7; i++) {
-        if (i % 2 == 0) {
-            soma += ean[i] * 3;  // Multiplica os dígitos nas posições ímpares por 3
-        } else {
-            soma += ean[i];      // Multiplica os dígitos nas posições pares por 1
+    for (int i = 0; i < 7; i++)
+    {
+        if (i % 2 == 0)
+        {
+            soma += ean[i] * 3; // Multiplica os dígitos nas posições ímpares por 3
+        }
+        else
+        {
+            soma += ean[i]; // Multiplica os dígitos nas posições pares por 1
         }
     }
-    
+
     // Calcula o dígito verificador
     int digitoVerificador = (10 - (soma % 10)) % 10;
-    
+
     return digitoVerificador;
 }
-
-//OIIIIIIIIII
-//olá
