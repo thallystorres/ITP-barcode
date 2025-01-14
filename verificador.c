2e00517c150f6c08e241f0509ce90381837348f2
joaoMiguelSousaOliveira <jjoaomiguel05@gmail.com>
2025-01-14 19:37:58 -0300

    Comentario Thallys Miguel

--- verificador.c
+++ verificador.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <string.h>
 
-// Função para converter um int em array
-
+// Função para converter uma string em array
 void str_array(char *str, int *array) 
 {
     for (int i = 0; i < 7; i++) 
@@ -21,37 +19,13 @@ int calcularDigitoVerificador(int ean[7])
 
     // Calcula a soma dos dígitos com os pesos
     for (int i = 0; i < 7; i++)
-=======
-#include "definitions.h"
-    // Função para calcular o dígito verificador
-    int verificarDigito(int ean[8])
->>>>>>> d06a6b856052827c7c39706e825ea5c046f930f4
     {
-        int soma = 0;
-
-        // Calcula a soma dos dígitos com os pesos
-        for (int i = 0; i < 7; i++)
+        if (i % 2 == 0)
         {
-            if (i % 2 == 0)
-            {
-                soma += ean[i] * 3; // Multiplica os dígitos nas posições ímpares por 3
-            }
-            else
-            {
-                soma += ean[i]; // Multiplica os dígitos nas posições pares por 1
-            }
-        }
-
-        // Calcula o dígito verificador
-        int digitoVerificador = (10 - (soma % 10)) % 10;
-        //Se o dígito verificador não estiver de acordo com o cálculo, retorne 1 para verdadeiro ou 0 para falso
-        if(digitoVerificador == ean[7])
-        {
-            return 1;
+            soma += ean[i] * 3; // Multiplica os dígitos nas posições ímpares por 3
         }
         else
         {
-<<<<<<< HEAD
             soma += ean[i]; // Multiplica os dígitos nas posições pares por 1
         }
     }
@@ -77,21 +51,16 @@ int main(){
     // Definimos "verificador" como o valor retornado pela função
     verificador = calcularDigitoVerificador(array);
 
-    // Atribuimos o valor do dígito verificador á última casa do nosso vetor
+    // Atribuimos o valor do dígito verificador à última casa do nosso vetor
     array[7] = verificador;
 
-    
+    //TESTES
+    /*
    for (int j = 0; j < 8; j++)
    {
         printf("%d", array[j]);
    }
-   /*
-    printf("%d", k);
-   */
+    */
+   
     return 0;
 }
-=======
-            return 0;
-        };
-    }
->>>>>>> d06a6b856052827c7c39706e825ea5c046f930f4
